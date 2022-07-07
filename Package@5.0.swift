96bfdb11dc445a304cd5cb2ea9090eb44a7dd40a
Mohamed Rebin K <45125106+rebink@users.noreply.github.com>
2022-07-07 12:38:35 +0530

    changes in package

--- Package@5.0.swift
+++ Package@5.0.swift
@@ -34,7 +34,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "secp256k1"),
         .target(
             name: "web3swift",
             dependencies: ["BigInt", "secp256k1", "PromiseKit", "Starscream", "CryptoSwift"],
